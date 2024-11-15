731e83b6bda372bd56d666061decc75f27f5135f
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2024-11-15 05:44:49 +0000

    calculating frequency of words and characters

--- File_Handling/cal_freq_words_chars.py
+++ File_Handling/cal_freq_words_chars.py
@@ -1,14 +1,17 @@
-#
+# calculate the No of words and chars
 
-file = open("C:\\Users\\suhel\\Desktop\\Pyhton-chai\\File_Handling\\file.txt", "r+")
+file = open("file.txt", "r+")
 
 new_list = []
-lines = file.readlines()
+lines = file.readlines() # this is list of lines from the file
+
+print(lines)
 str = ""
 for i in lines:
    str = str + i
 print(str)
 
+# ls = str.split('\n')
 ls = str.split()
 
 print(ls)
