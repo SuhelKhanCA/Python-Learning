877ee1f4d32b97e84f1f0fa448d881c2ba131fb9
AHMAD SHADAB <162570930+Aliggp6444@users.noreply.github.com>
2024-12-07 17:53:58 +0000

    global variable example

---
+++ local-gobal-vars/global_variable_01.py
@@ -0,0 +1,13 @@
+# Global variable example
+
+a=1
+
+def myfucntion():
+
+    b = 2
+    print("a = ", a)
+    print("b = ", b)
+
+myfucntion()
+
+print("a = ",a) # a is accessible as it a global variable
